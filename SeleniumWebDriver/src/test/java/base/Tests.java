6e0d5987884ee6b8cfae9ddc2b31ddaff365a449
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-11-01 16:46:05 +0100

    Update Tests.java

--- SeleniumWebDriver/src/test/java/base/Tests.java
+++ SeleniumWebDriver/src/test/java/base/Tests.java
@@ -2,21 +2,28 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import pages.HomePage;
+
 
 public class Tests {
 
     private WebDriver driver;
     protected HomePage homePage;
 
+    @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\liene\\Desktop\\TA\\Selenium\\SeleniumWebDriver\\resources\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
-        homePage= new HomePage(driver);
+        homePage = new HomePage(driver);
+    }
 
-
+    @AfterClass
+    public void tearDown() {
         driver.quit();
     }
+
 }
