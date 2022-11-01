71107c85ee608ea00af665d8c09e9bb597e302a8
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-11-01 15:25:06 +0100

    Create HomePage.java

---
+++ SeleniumWebDriver/src/main/java/pages/HomePage.java
@@ -0,0 +1,21 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+    private By formAuthenticationLink = By.linkText("Form Authentication");
+    private WebDriver driver;
+
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+
+    }
+
+    public LoginPage clickFormAuthenticationLink() {
+        driver.findElement(formAuthenticationLink).click();
+        return new LoginPage(driver);
+    }
+}
