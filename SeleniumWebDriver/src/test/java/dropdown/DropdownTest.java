440e8bf8d391e42f072358e2fb7d0ccc15050159
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-11-02 11:51:00 +0100

    Create DropdownTest.java

---
+++ SeleniumWebDriver/src/test/java/dropdown/DropdownTest.java
@@ -0,0 +1,25 @@
+package dropdown;
+
+
+import base.Tests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class DropdownTest extends Tests {
+
+    @Test
+    public void testSelectOption() {
+        var dropdownPage = homePage.clickDropDown();
+        String option = "Option 1";
+
+        dropdownPage.selectFromDropdown(option);
+        var selectedOptions = dropdownPage.getSelectedOptions();
+        assertEquals(selectedOptions.size(),1, "Incorrect number of selections");
+        Assert.assertTrue(selectedOptions.contains(option), "Option not selected");
+
+    }
+
+}
