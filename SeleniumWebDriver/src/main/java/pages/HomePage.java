454de9e387994d94deeb4bb4a2b823be6282879a
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-11-02 16:42:23 +0100

    Update HomePage.java

--- SeleniumWebDriver/src/main/java/pages/HomePage.java
+++ SeleniumWebDriver/src/main/java/pages/HomePage.java
@@ -24,6 +24,11 @@ public class HomePage {
         return new DropdownPage(driver);
     }
 
+    public ForgotPasswordPage clickForgotPassword(){
+        clickLink("Forgot Password");
+        return  new ForgotPasswordPage(driver);
+    }
+
     public void clickLink(String linkText) {
         driver.findElement(By.linkText(linkText)).click();
     }
