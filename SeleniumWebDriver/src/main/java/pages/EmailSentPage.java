1ecb01015c320cc2fbdef6f215321d814bd466a2
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-11-02 16:42:30 +0100

    Create EmailSentPage.java

---
+++ SeleniumWebDriver/src/main/java/pages/EmailSentPage.java
@@ -0,0 +1,21 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class EmailSentPage {
+
+    private WebDriver driver;
+    private By contentMessage = By.id("content");
+
+    public EmailSentPage(WebDriver driver) {
+        this.driver = driver;
+
+    }
+
+    public String confirmMessage(){
+        return driver.findElement(contentMessage).getText();
+    }
+
+
+}
