741a1f57e60d3fff2ebee506bd8ebda9f05960f9
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-11-02 16:42:18 +0100

    Update DropdownTest.java

--- SeleniumWebDriver/src/test/java/dropdown/DropdownTest.java
+++ SeleniumWebDriver/src/test/java/dropdown/DropdownTest.java
@@ -6,7 +6,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+
 
 public class DropdownTest extends Tests {
 
