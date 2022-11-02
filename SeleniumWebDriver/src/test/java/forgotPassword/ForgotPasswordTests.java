cffd6d5fbdd62c6519fc63ec985c51342d1eeec6
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-11-02 16:42:09 +0100

    Update ForgotPasswordTests.java

--- SeleniumWebDriver/src/test/java/forgotPassword/ForgotPasswordTests.java
+++ SeleniumWebDriver/src/test/java/forgotPassword/ForgotPasswordTests.java
@@ -2,6 +2,21 @@ package forgotPassword;
 
 import base.Tests;
 import org.testng.annotations.Test;
+import pages.EmailSentPage;
+import pages.ForgotPasswordPage;
+
+import static org.testng.AssertJUnit.assertEquals;
+
 
 public class ForgotPasswordTests extends Tests {
+
+    @Test
+    public void testForgotPassword() {
+        ForgotPasswordPage forgotPasswordPage = homePage.clickForgotPassword();
+        forgotPasswordPage.enterEmail("test123@example.com");
+        EmailSentPage emailSentPage = forgotPasswordPage.clickRetrievePasswordButton();
+        assertEquals(emailSentPage.confirmMessage(), "Your e-mail's been sent!", "Message is incorrect");
+
+
+    }
 }
