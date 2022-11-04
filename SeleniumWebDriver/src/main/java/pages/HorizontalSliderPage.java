b5962c395330db11208205bf700837fed2e755b4
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-11-04 14:41:57 +0100

    Update HorizontalSliderPage.java

--- SeleniumWebDriver/src/main/java/pages/HorizontalSliderPage.java
+++ SeleniumWebDriver/src/main/java/pages/HorizontalSliderPage.java
@@ -1,17 +1,30 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class HorizontalSliderPage {
 
     private WebDriver driver;
-    public By slider = By.className("sliderContainer");
+    public By slider = By.cssSelector(".sliderContainer input");
+    public By index = By.id("range");
+
 
     public HorizontalSliderPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    
+
+    public void setSliderValue(String value) {
+        while (!getSliderValue().equals(value)) {
+            driver.findElement(slider).sendKeys(Keys.ARROW_RIGHT);
+        }
+    }
+
+    public String getSliderValue() {
+        return driver.findElement(index).getText();
+    }
 }
