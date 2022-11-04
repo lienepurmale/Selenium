2f9972d4b169a67d43fcc9757b6326f27a04eaf9
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-11-04 13:18:58 +0100

    Create HorizontalSliderTest.java

---
+++ SeleniumWebDriver/src/test/java/horizontalSlider/HorizontalSliderTest.java
@@ -0,0 +1,13 @@
+package horizontalSlider;
+
+import base.Tests;
+import org.testng.annotations.Test;
+
+public class HorizontalSliderTest extends Tests {
+
+    @Test
+    public void testHorizontalSlider(){
+        var horizontalPage = homePage.clickHorizontalSlider();
+
+    }
+}
