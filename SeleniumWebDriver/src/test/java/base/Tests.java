c7b0dc3ebc74158c8c86316baccc250288b45355
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-11-07 13:38:57 +0100

    Update Tests.java

--- SeleniumWebDriver/src/test/java/base/Tests.java
+++ SeleniumWebDriver/src/test/java/base/Tests.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 
 
@@ -16,9 +17,15 @@ public class Tests {
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\liene\\Desktop\\TA\\Selenium\\SeleniumWebDriver\\resources\\chromedriver.exe");
         driver = new ChromeDriver();
+        goHome();
         driver.get("https://the-internet.herokuapp.com/");
 
         homePage = new HomePage(driver);
+    }
+ //To make sure that Tests in AlertTests page go to the homepage after each test
+    @BeforeMethod
+    public void goHome(){
+        driver.get("https://the-internet.herokuapp.com/");
     }
 
     @AfterClass
