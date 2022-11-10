f233953de58b2fcf8392f870e059b7bb4ae29694
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-11-10 14:56:59 +0100

    Create HiddenElementPage.java

---
+++ SeleniumWebDriver/src/main/java/pages/HiddenElementPage.java
@@ -0,0 +1,33 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class HiddenElementPage {
+
+    private WebDriver driver;
+    private By startButton = By.cssSelector("#start button");
+    private By loadingIndicator = By.id("loading");
+    private By text = By.id("finish");
+
+    public HiddenElementPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickStartButton() {
+        driver.findElement(startButton).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(7));
+        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(loadingIndicator)));
+    }
+
+    public String getLoadedText() {
+        return driver.findElement(text).getText();
+    }
+
+
+}
