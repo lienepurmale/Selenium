6e5af1f37b7818804bc20da186a974be0c81ef0f
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-11-11 14:32:19 +0100

    Update Tests.java

--- SeleniumWebDriver/src/test/java/base/Tests.java
+++ SeleniumWebDriver/src/test/java/base/Tests.java
@@ -6,6 +6,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.WindowManagement;
 
 import java.util.concurrent.TimeUnit;
 
@@ -34,6 +35,10 @@ public class Tests {
     @AfterClass
     public void tearDown() {
         driver.quit();
+    }
+
+    public WindowManagement getWindowManager(){
+        return new WindowManagement(driver);
     }
 
 }
