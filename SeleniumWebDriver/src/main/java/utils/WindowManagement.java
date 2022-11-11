868e961e6a8b80d450157bdd3b0322edba6f391f
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-11-11 14:32:25 +0100

    Create WindowManagement.java

---
+++ SeleniumWebDriver/src/main/java/utils/WindowManagement.java
@@ -0,0 +1,30 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+
+public class WindowManagement {
+
+    private WebDriver driver;
+    private WebDriver.Navigation navigate;
+
+    public WindowManagement(WebDriver driver){
+        this.driver = driver;
+        navigate = driver.navigate();
+    }
+
+    public void goBack(){
+        navigate.back();
+    }
+
+    public void goForward(){
+        navigate.forward();
+    }
+
+    public void refreshPage(){
+        navigate.refresh();
+    }
+
+    public void goTo(String url){
+        navigate.to(url);
+    }
+}
